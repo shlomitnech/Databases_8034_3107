ff4b69a42b851afee3e8244c237730d879520a43
shlomitnech <98982263+shlomitnech@users.noreply.github.com>
2024-07-23 17:29:48 +0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,4 @@
-
+--Query 1
 --to get the Reader ID and names of readers who have lost or damaged cards
 SELECT r."ReaderID", r."Name"
 FROM public."Reader" r
@@ -6,7 +6,9 @@ JOIN public."ReaderCard" rc ON r."ReaderID" = rc."ReaderID"
 WHERE rc."LostDamaged_Status" IN ('lost', 'damaged');
 --501.631 ms
 --After indexing: 315.402 ms
+Select get_readers_with_lost_damaged_cards()
 
+--Query 2
 --to get the Reader ID, names and ages of children under the age of 6
 SELECT r."ReaderID", r."Name", c."Age"
 FROM public."Reader" r
@@ -14,8 +16,9 @@ JOIN public."Child" c ON r."ReaderID" = c."ReaderID"
 WHERE c."Age" < 6;
 --72.933 ms
 --After Indexing: 55.869 ms
+Select get_children_under_age_6()
 
-
+--Query 3
 --to get the reader ID, name and title of book with which the authors name is Michael Jackson
 SELECT r."ReaderID", r."Name", b."Title"
 FROM public."Reader" r
@@ -25,6 +28,7 @@ WHERE b."Author" = 'Michael Jackson';
 --160.171 ms
 --After indexing: 19.918 ms
 
+--Query 4
 --to get the family id, phone and email of families whos' size is greater than or equal to 4 and have more than 2 children
 SELECT f."FamilyID", f."FamilyPhone", f."FamilyEmail", 
        COUNT(a."ReaderID") + COUNT(c."ReaderID") AS "MemberCount",
@@ -37,7 +41,9 @@ HAVING COUNT(a."ReaderID") + COUNT(c."ReaderID") >= 4
    AND COUNT(c."ReaderID") > 2;
 --855.246 ms
 --After indexing: 535.345 ms
+Select get_large_families()
 
+--Query 5
 ----------To update the Student's ReaderCards to be Premium and NoStatus in terms of lost or damaged
 UPDATE public."ReaderCard" rc
 SET "CardType" = 'premium', "LostDamaged_Status" = 'NoStatus'
@@ -53,7 +59,7 @@ WHERE rc."ReaderID" = s."ReaderID"
 -- JOIN public."Student" s ON rc."ReaderID" = s."ReaderID"
 -- WHERE s."Institution" = 'university';
 
-
+--Query 6
 ---------To update all the due dates for children under the age of 5 by 3 days
 UPDATE public."Loan" l
 SET "DueDate" = "DueDate" + INTERVAL '3 days'
@@ -71,6 +77,7 @@ WHERE l."ReaderID" = c."ReaderID"
 -- JOIN public."Reader" r ON l."ReaderID" = r."ReaderID"
 -- WHERE c."Age" < 10;
 
+--Query 7
 ---------deletes all the loans from students that are past their due date
 DELETE FROM public."Loan"
 USING public."Student"
@@ -78,29 +85,32 @@ WHERE public."Loan"."ReaderID" = public."Student"."ReaderID"
 AND public."Loan"."DueDate" < current_date;
 --120.755 ms
 
+--Query 8
 ----------Delete the readers (and their readercard) of all children readers >= 18
 -- Begin transaction
 DELETE FROM public."Child"
 WHERE "Age" >= 18;
 --22.261 ms
 
+--Query 9
 ---- Delete all children with FamilyID 57250737
 DELETE FROM public."Child"
 WHERE "FamilyID" = 57250737;
 --22.368 ms
 
+--Query 10
 -- Delete all adults with FamilyID 57250737
 DELETE FROM public."Adult"
 WHERE "FamilyID" = 57250737;
 -- 32.820 ms
 
-
+--Query 11
 -- Delete the family with FamilyID 57250737
 DELETE FROM public."Family"
 WHERE "FamilyID" = 57250737;
 --13.886 ms
 
-
+--Query 12
 --. Select all books loaned by children (under the age of 18) with their due dates and the names of the children.
 SELECT b."Title", b."Author", l."DueDate", r."Name" AS "ChildName", c."Age"
 FROM public."Book" b
@@ -110,6 +120,7 @@ JOIN public."Child" c ON r."ReaderID" = c."ReaderID"
 WHERE c."Age" < 18;
 --time: 590 ms
 
+--Query 13
 --Select all families along with the number of adult members and children they have.
 SELECT f."FamilyID", f."FamilyEmail", f."FamilyPhone", 
        COUNT(DISTINCT a."ReaderID") AS "NumAdults", 
@@ -120,6 +131,7 @@ LEFT JOIN public."Child" c ON f."FamilyID" = c."FamilyID"
 GROUP BY f."FamilyID", f."FamilyEmail", f."FamilyPhone";
 --time: 1589 ms
 
+--Query 14
 --Students ID, their cards and their loan numbers
 SELECT s."ReaderID", s."StudentID", rc."CardNumber",
        l."TransactionID", l."BookID"
