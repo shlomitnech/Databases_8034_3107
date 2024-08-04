bc7fc2636cb6ba964c435691d44421bf8ca4e481
navamandel <117572528+navamandel@users.noreply.github.com>
2024-08-04 08:20:34 +0300

    Add files via upload

--- Drop.sql
+++ Drop.sql
@@ -1,54 +1,54 @@
 --
 -- Drop Table    : 'Adult'   
 --
-DROP TABLE if EXISTS "Adult";
+DROP TABLE if EXISTS "Adult" CASCADE;
 
 --
 -- Drop Table    : 'Book'   
 --
-DROP TABLE if EXISTS "Book";
+DROP TABLE if EXISTS "Book" CASCADE;
 
 --
 -- Drop Table    : 'Loan'   
 --
-DROP TABLE if EXISTS "Loan";
+DROP TABLE if EXISTS "Loan" CASCADE;
 
 --
 -- Drop Table    : 'Child'   
 --
-DROP TABLE if EXISTS "Child";
+DROP TABLE if EXISTS "Child" CASCADE;
 
 --
 -- Drop Table    : 'Family'   
 --
-DROP TABLE if EXISTS "Family";
+DROP TABLE if EXISTS "Family" CASCADE;
 
 --
 -- Drop Table    : 'Reader'   
 --
-DROP TABLE if EXISTS "Reader";
+DROP TABLE if EXISTS "Reader" CASCADE;
 
 --
--- Drop Table    : 'RaederCard'   
+-- Drop Table    : 'ReaderCard'   
 --
-DROP TABLE if EXISTS "ReaderCard";
+DROP TABLE if EXISTS "ReaderCard" CASCADE;
 
 
 --
 -- Drop Table    : 'Student'   
 --
-DROP TABLE if EXISTS "Student";
+DROP TABLE if EXISTS "Student" CASCADE;
 
 
 
-DROP TYPE IF EXISTS lost_damaged_status;
-DROP TYPE IF EXISTS card_type;
-DROP TYPE IF EXISTS institution_type;
+DROP TYPE IF EXISTS lost_damaged_status CASCADE;
+DROP TYPE IF EXISTS card_type CASCADE;
+DROP TYPE IF EXISTS institution_type CASCADE;
 
 
 
-DROP SEQUENCE Book_BookID_seq;
-DROP SEQUENCE Family_FamilyID_seq;
-DROP SEQUENCE Loan_TransactionID_seq;
-DROP SEQUENCE Reader_ReaderID_seq;
-DROP SEQUENCE ReaderCard_CardNumber_seq;
+DROP SEQUENCE IF EXISTS Book_BookID_seq CASCADE;
+DROP SEQUENCE IF EXISTS Family_FamilyID_seq CASCADE;
+DROP SEQUENCE IF EXISTS Loan_TransactionID_seq CASCADE;
+DROP SEQUENCE IF EXISTS Reader_ReaderID_seq CASCADE;
+DROP SEQUENCE IF EXISTS ReaderCard_CardNumber_seq CASCADE;
