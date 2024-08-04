f879f16bfff1cd7ee8605996230de4a71fef4d92
shlomitnech <98982263+shlomitnech@users.noreply.github.com>
2024-08-04 07:47:48 +0300

    Update ViewDBLink.sql

--- Merged (Stage 4)/ViewDBLink.sql
+++ Merged (Stage 4)/ViewDBLink.sql
@@ -1,6 +1,5 @@
 --View # 1 
-
-
+--View all readers and their loans, and loan info
 CREATE OR REPLACE VIEW combined_reader_loans_books AS
 SELECT
     R."ReaderID",
@@ -62,3 +61,54 @@ WHERE authorid IN (
 
 INSERT INTO public."Loan" ("TransactionID", "BorrowDate", "DueDate", "BookID", "ReaderID")
 VALUES (1, '2024-08-02', '2024-08-09', 1, 1);
+
+--View 2 : Students and their loan info
+CREATE OR REPLACE VIEW student_loans_books AS
+SELECT
+    S."ReaderID",
+    S."StudEmail",
+    S."Institution",
+    S."StudentID",
+    L."TransactionID",
+    L."BorrowDate",
+    L."DueDate",
+    B.bookid,
+    B.title,
+    B.genre,
+    B.condition
+FROM
+    public."Student" S
+JOIN
+    public."Loan" L ON S."ReaderID" = L."ReaderID"
+JOIN 
+    dblink('dbname=Book user=postgres password=Hadas1994', 
+           'SELECT bookid, title, genre, condition FROM book') 
+    AS B(bookid INT, title TEXT, genre TEXT, condition TEXT)
+ON 
+    L."BookID" = B.bookid;
+
+
+SELECT *
+FROM student_loans_books
+WHERE "StudentID" = '"91045522"';
+
+SELECT *
+FROM student_loans_books
+WHERE genre = 'Fiction';
+
+
+
+UPDATE book
+SET condition = 'Excellent'
+WHERE bookid IN (
+    SELECT bookid
+    FROM student_loans_books
+    WHERE bookid = 1
+);
+
+INSERT INTO public."Loan" ("TransactionID", "BorrowDate", "DueDate", "BookID", "ReaderID")
+VALUES (2, '2024-08-02', '2024-08-09', 2, (
+    SELECT "ReaderID"
+    FROM public."Student"
+    WHERE "StudentID" = '"91045522"'
+));
